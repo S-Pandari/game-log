da85f5c17a33259a06df33ae2acd5a371a7ebc7c
Srujan Pandari <31961925+S-Pandari@users.noreply.github.com>
2023-02-11 15:13:50 -0800

    Added basic files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Box, Toolbar} from '@mui/material'
+
+function NavBar() {
+  return ( 
+    <Box>
+      <AppBar>
+        <Toolbar>
+
+        </Toolbar>
+      </AppBar> 
+    </Box>
+    );
+}
+
+export default NavBar;
