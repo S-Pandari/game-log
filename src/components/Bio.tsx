69169252c7acbaa56a0582ae2fd85544f1f4392b
Srujan Pandari <31961925+S-Pandari@users.noreply.github.com>
2023-02-11 21:24:29 -0500

    finished function for API calls

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,4 +1,5 @@
 import { AppBar, Box, Toolbar} from '@mui/material'
+import React from 'react';
 
 function Bio() {
   return ( 
---
+++ src/utils/RequestAPI.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export function GetData(url : string, path : string, key : string, setData : Function) {
+
+    axios.get(`${url}/${path}${key == '' ? '' : '?key=' + key}`)
+        .then(function (response : any) {
+        setData(response);
+    })
+        .catch(function (error : any) {
+        // handle error
+        console.log(error);
+    });
+
+}
